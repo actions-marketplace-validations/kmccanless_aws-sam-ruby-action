36f486662b074067abf37385d6df8cc8c79677ec
Keith McCanless <keith@keiths-mbp.lan>
2020-06-05 07:15:38 -0400

    debuggin

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,6 +36,8 @@ if [ -z "$CAPABILITIES" ] && [ "$INPUT_SAM_COMMAND" == "deploy" ]; then
 fi
 #echo "setting up ruby"
 #PATH=$PATH:~/.rbenv/shims
+echo "PWD is $PWD"
+#cd "$GITHUB_WORKSPACE"
 echo "Running SAM commands"
 if [ "$INPUT_SAM_COMMAND" == "build" ]; then
 		echo "Running sam build"
