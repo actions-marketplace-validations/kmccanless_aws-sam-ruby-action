deecdd73c7b624868127462cc6b148df4fb79e0b
Keith McCanless <keith@keiths-mbp.lan>
2020-06-05 05:59:14 -0400

    added deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,22 +3,34 @@
 set -e
 echo "Validating inputs"
 if [[ -z "$INPUT_SAM_COMMAND" ]]; then
-    echo AWS Stack Name invalid
+    echo "AWS SAM command invalid"
     exit 1
 fi
 
 if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
-    echo AWS Access Key ID invalid
+    echo "AWS Access Key ID invalid"
     exit 1
 fi
 
 if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
-    echo AWS Secret Access Key invalid
+    echo "AWS Secret Access Key invalid"
     exit 1
 fi
 
 if [[ -z "$AWS_REGION" ]]; then
-    echo AWS Region invalid
+    echo "AWS Region invalid"
+    exit 1
+fi
+if [[ -z "$STACK_NAME" ]]; then
+    echo "Stack Name Invalid"
+    exit 1
+fi
+if [[ -z "$S3_BUCKET" ]]; then
+    echo "S3 Bucket Invalid"
+    exit 1
+fi
+if [[ -z "$CAPABILITIES" ]]; then
+    echo "Capabilities Invalid"
     exit 1
 fi
 echo "setting up ruby"
@@ -29,4 +41,9 @@ cd
 if [ "${INPUT_SAM_COMMAND}" == "build" ]; then
 		echo "Running sam build"
 		sam build
+fi
+
+if [ "${INPUT_SAM_COMMAND}" == "deploy" ]; then
+		echo "Running sam build"
+		sam deploy --stack-name "${STACK_NAME}" --s3-bucket "${S3_BUCKET}" --capabilities "${CAPABILITIES}"
 fi
