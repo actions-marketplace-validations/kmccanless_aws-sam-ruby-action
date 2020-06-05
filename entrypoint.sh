fc682a0dd2d1a0009a0236e380472cea32e4810a
Keith McCanless <keith@keiths-mbp.lan>
2020-06-05 07:08:35 -0400

    adding shims to PATH

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,9 +34,8 @@ if [ -z "$CAPABILITIES" ] && [ "$INPUT_SAM_COMMAND" == "deploy" ]; then
     echo "Capabilities Invalid"
     exit 1
 fi
-#echo "setting up ruby"
-#source ~/.bash_profile
-#rbenv local 2.7.0
+echo "setting up ruby"
+PATH=$PATH:~/.rbenv/shims
 echo "Running SAM commands"
 if [ "$INPUT_SAM_COMMAND" == "build" ]; then
 		echo "Running sam build"
